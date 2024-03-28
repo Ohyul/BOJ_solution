7b3917ac090826866c61aa8cd38b3d3e23eccccc
Ohyul_Kweon <74843792+Ohyul@users.noreply.github.com>
2024-03-28 16:57:35 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[101][101];
+int n, m;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int val;
+			cin >> val;
+			arr[i][j] += val;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
