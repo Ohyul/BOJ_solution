f07ef9bfb9c86baf3563751bb6963f0fa552b078
Ohyul_Kweon <74843792+Ohyul@users.noreply.github.com>
2024-03-29 16:43:11 +0900

    [Gold I] Title: 가계부 (Hard), Time: 68 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+typedef long long ll;
+using namespace std;
+
+ll n, m;
+ll arr[1000010], segTree[1000010 * 4];
+ll strIdx;
+
+void init() {
+    strIdx = 1;
+    while (strIdx < n) strIdx <<= 1;
+    for (ll i = 0; i < n; i++) {
+        segTree[i + strIdx] = arr[i];
+    }
+    for (ll i = strIdx - 1; i >= 1; i--) {
+        segTree[i] = segTree[i << 1] + segTree[i << 1 | 1];
+    }
+}
+
+void query(ll from, ll to) {
+    ll sum = 0;
+    from += strIdx;
+    to += strIdx;
+    for (from, to; from != to; from >>= 1, to >>= 1) {
+        if (from & 1) {
+            sum += segTree[from];
+            from++;
+        }
+        if (to & 1) {
+            to--;
+            sum += segTree[to];
+        }
+    }
+    cout << sum << "\n";
+}
+
+void update(ll idx, ll val) {
+    arr[idx] += val;
+    idx += strIdx;
+    segTree[idx] = arr[idx - strIdx];
+    idx >>= 1;
+    while (idx >= 1) {
+        segTree[idx] = segTree[idx << 1] + segTree[(idx << 1) | 1];
+        idx >>= 1;
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    init();
+    for (ll i = 0; i < m; i++) {
+        ll inst;
+        cin >> inst;
+        if (inst == 2) {
+            ll from, to;
+            cin >> from >> to;
+            query(from - 1, to);
+        }
+        if (inst == 1) {
+            ll idx, val;
+            cin >> idx >> val;
+            update(idx - 1, val);
+        }
+    }
+}
