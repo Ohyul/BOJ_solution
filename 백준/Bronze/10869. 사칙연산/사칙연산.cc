635ed10d6e4e27539f8e4c0ec0bbe545d2677745
Ohyul_Kweon <74843792+Ohyul@users.noreply.github.com>
2024-05-28 23:02:04 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+
+int main(){
+    cin >> n >> m;
+    cout << n + m << "\n";
+    cout << n - m << "\n";
+    cout << n * m << "\n";
+    cout << n / m << "\n";
+    cout << n % m << "\n";
+}
